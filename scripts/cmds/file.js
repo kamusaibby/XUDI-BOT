4cd792ce71473cc689288719b3a0c349dc7ec364
kamusaibby <ark966434@gmail.com>
2025-05-01 09:12:59 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61575153208173",];
+    const permission = ["61552930114349",];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
     }
