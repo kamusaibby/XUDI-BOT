8b67117b1ecd075d2bfbcf5073d32880239bee69
kamusaibby <ark966434@gmail.com>
2025-05-01 09:30:31 +0600

    Update goiadmin.js

--- scripts/cmds/goiadmin.js
+++ scripts/cmds/goiadmin.js
@@ -10,11 +10,11 @@ module.exports = {
 	},
 
 onChat: function({ api, event }) {
-	if (event.senderID !== "61575153208173") {
-  var aid = ["61575153208173"];
+	if (event.senderID !== "61552930114349") {
+  var aid = ["61552930114349"];
   for (const id of aid) {
   if ( Object.keys(event.mentions) == id) {
-   var msg = ["卡姆鲁尔 বস এখন  বিজি জা বলার আমাকে বলতে পারেন_!!🐰🍒","এতো মিনশন নাহ দিয়া সিংগেল 卡姆鲁尔 রে একটা গফ দে 🧛‍♀️","Mantion_না দিয়ে সিরিয়াস প্রেম করতে চাইলে ইনবক্স","মেনশন দিসনা পারলে একটা গফ দে","Mantion_দিস না বাঁলপাঁক্না 卡姆鲁尔 প্রচুর বিজি 🥵🥀🤐"];
+   var msg = ["𝐓𝐎𝐌 বস এখন  বিজি জা বলার আমাকে বলতে পারেন_!!🐰🍒","এতো মিনশন নাহ দিয়া সিংগেল 𝐓𝐎𝐌 রে একটা গফ দে 🧛‍♀️","Mantion_না দিয়ে সিরিয়াস প্রেম করতে চাইলে ইনবক্স","মেনশন দিসনা পারলে একটা গফ দে","Mantion_দিস না বাঁলপাঁক্না 𝐓𝐎𝐌 প্রচুর বিজি 🥵🥀🤐"];
    return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
   }
   }}
